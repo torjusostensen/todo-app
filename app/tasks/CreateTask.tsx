9e56153c65c1108ba7eaf9517cf3059142a53316
torjusostensen <torjusostensen@gmail.com>
2023-08-20 15:21:37 +0200

    func to add tasks, issue with fetching is not resolved

---
+++ app/tasks/CreateTask.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function HandleTask() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const router = useRouter();
+
+  const create = async() => {
+
+    await fetch('http://127.0.0.1:8090/api/collections/tasks/records', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+        content,
+      }),
+    });
+
+    setContent('');
+    setTitle('');
+
+    router.refresh();
+  }
+
+  return (
+    <form onSubmit={create}>
+      <h3>Create a new Task</h3>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+        maxLength={10}
+      />
+      <textarea
+        placeholder="Content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        required
+        maxLength={20}
+      />
+      <button type="submit">
+        Create Task
+      </button>
+    </form>
+  );
+}
